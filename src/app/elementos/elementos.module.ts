bb986021a9caa52c786d7044718daad2264683ba
David Malpica <dmalpica96@hotmail.com>
2022-12-20 12:58:57 -0600

    Exports dado component

--- src/app/elementos/elementos.module.ts
+++ src/app/elementos/elementos.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DadoComponent } from './dado/dado.component';
 
 
-
 @NgModule({
   declarations: [
     DadoComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [DadoComponent],
 })
 export class ElementosModule { }
